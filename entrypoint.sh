9a82ce5969e2ad03db185b16f3736a6a21b1a1dc
SJM <sjm@opusvl.com>
2025-04-05 11:57:08 +0200

    updated main path to new layout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-RUNFILE=/srv/hps/scripts/run-hps.sh
+RUNFILE=/srv/hps-system/scripts/run-hps.sh
 
 if [[ ! -x $RUNFILE ]]; then
   echo '[ERROR] $0 Required file $RUNFILE is missing or not executable.' >&2
   echo '[ERROR] $0 It looks like the hps-system is not installed.' >&2
-  echo '[ERROR] $0 Please extract hps-system into /srv/hps for this container to function.' >&2
+  echo '[ERROR] $0 Please extract hps-system into /srv/hps-system for this container to function.' >&2
   sleep 2
   exit 1
 fi
